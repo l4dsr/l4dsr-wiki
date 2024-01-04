9514ef9221a4efbe656835ed41b1269a99e6be24
Hayden K <imaciidz@gmail.com>
2024-01-04 02:05:49 -0500

    Remove default homepage description (to be changed)
    Will reintroduce this later when there's something actually worth
    putting here

--- src/pages/index.js
+++ src/pages/index.js
@@ -33,7 +33,7 @@ export default function Home() {
   return (
     <Layout
       title={`Welcome!`}
-      description="Description will go into a meta tag in <head />">
+      description=" <head />">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
